ccb57dc0a18055f2e9d0299f85e9693addb96bf1
Klodian Behrami <klodianbehrami@gmail.com>
2023-08-15 14:33:55 -0400

    twoSum monkeypatch

--- practice-for-ch-js-intro-js-exercises-long-practice-main/practice-for-ch-js-intro-js-exercises-long-practice-main/phase-1-arrays.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/practice-for-ch-js-intro-js-exercises-long-practice-main/phase-1-arrays.js
@@ -1,15 +1,37 @@
 // Your code here
 
 Array.prototype.uniq = function(){
-  result = [];
+  let result = [];
   let i = 0;
   while ( i < this.length) {
     if (!result.includes(this[i])) {
       result.push(this[i])
     };
 
+    i++ 
+
   };
   return result;
 };
 
-[1, 1, 2, 2, 3, 3].uniq();
+// console.log([1, 1, 2, 2, 3, 3].uniq());
+
+Array.prototype.twoSum = function() {
+  let result = []; 
+  let i = 0;
+  while (i < this.length - 1) {
+    let y = i + 1;
+    while (y < this.length) {
+      if (this[i] + this[y] === 0) {
+        result.push([i, y]);
+      } 
+      y++;
+    }
+    i++;
+  }
+  return result;
+}
+
+// console.log([-1, 0, 2, -2, 1].twoSum())
+
+
