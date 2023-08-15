690a94985f19f0d73baa5444f1f779bb7d78c8cc
tanziti <tanzitian3@gmail.com>
2023-08-15 15:30:24 -0400

    myMap

--- practice-for-ch-js-intro-js-exercises-long-practice-main/practice-for-ch-js-intro-js-exercises-long-practice-main/phase-1-arrays.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/practice-for-ch-js-intro-js-exercises-long-practice-main/phase-1-arrays.js
@@ -34,4 +34,21 @@ Array.prototype.twoSum = function() {
 
 // console.log([-1, 0, 2, -2, 1].twoSum())
 
+Array.prototype.transpose = function(){
+  let result = []; 
+  let i = 0;
 
+  while (i < this.length) {
+      let j = 0; 
+      subarr = [];
+    while (j < this.length) {
+    subarr.push(this[j][i])
+    j++;
+    }
+    result.push(subarr);
+    i++;
+  }
+  return result;
+}
+
+console.log([[0, 1, 2], [3, 4, 5], [6, 7, 8]].transpose())
--- practice-for-ch-js-intro-js-exercises-long-practice-main/practice-for-ch-js-intro-js-exercises-long-practice-main/phase-2-enumerable.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/practice-for-ch-js-intro-js-exercises-long-practice-main/phase-2-enumerable.js
@@ -1,1 +1,36 @@
 // Your code here
+
+Array.prototype.myEach = function(callback) {
+  for (let i = 0; i < this.length; i++) {
+    callback(this[i]) 
+  }
+}
+
+function double(num) {
+  console.log(num * 2)
+  }
+// [1,2,3].myEach(double)
+
+Array.prototype.myMap = function(callback) {
+  let result = [];
+  for (let i = 0; i < this.length; i++) {
+    result.push(callback(this[i]));
+  }
+  return result;
+}
+
+[1,2,3].myMap(myEach)
+// [1,2,3].each do |ele|
+// end
+
+
+
+// def myeach(prc) 
+//   i = 0
+//   while i < self.length 
+//    prc.call(self[i]) 
+//  end   
+// end
+
+// double = Proc.new{|ele| puts ele * 2}
+// [1,2,3].myeach(double) 
