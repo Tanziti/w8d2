65fcfbaa510f9c81015c117166379c65fae51ddd
tanziti <tanzitian3@gmail.com>
2023-08-15 14:17:02 -0400

    first test

--- practice-for-ch-js-intro-js-exercises-long-practice-main/practice-for-ch-js-intro-js-exercises-long-practice-main/phase-1-arrays.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/practice-for-ch-js-intro-js-exercises-long-practice-main/phase-1-arrays.js
@@ -1,1 +1,15 @@
 // Your code here
+
+Array.prototype.uniq = function(){
+  result = [];
+  let i = 0;
+  while ( i < this.length) {
+    if (!result.includes(this[i])) {
+      result.push(this[i])
+    };
+
+  };
+  return result;
+};
+
+[1, 1, 2, 2, 3, 3].uniq();
